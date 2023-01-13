8e103eb02d5019b08219dc9e6456f5754afd62cf
AlinaSyniavska <alina22syniavska@gmail.com>
2023-01-14 01:53:37 +0200

    add debounce fn for send requests

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -5,9 +5,19 @@ import Input from '@mui/material/Input';
 
 const SearchBar: FC = () => {
 
+    const debounce = (fn: Function, ms = 500) => {
+        let timeoutId: ReturnType<typeof setTimeout>;
+        return function (this: any, ...args: any[]) {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => fn.apply(this, args), ms);
+        };
+    };
+
     const changeFilterKeywords = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value);
     }
+
+    const debouncedChangeFilterKeywords = debounce(changeFilterKeywords, 1000);
 
     return (
         <div>
@@ -16,7 +26,9 @@ const SearchBar: FC = () => {
                 <SearchIcon/>
                 <Input
                     placeholder={'Search...'}
-                    onChange={changeFilterKeywords}
+                    onChange={debouncedChangeFilterKeywords}
+                    fullWidth={true}
+                    disableUnderline={true}
                 />
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,9 @@ module.exports = {
         'item-shadow': '2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12)',
       },
       colors: {
-        // 'dark': '#3A4562',
+        'base-text': '#363636',
+        'filter-text': '#575757',
+        'border-color': '#EAEAEA',
       },
       screens: {
         '2xl': {'max': '1535px'},
