33307299a436b36c98258939ad1b323ce767638f
AlinaSyniavska <alina22syniavska@gmail.com>
2023-01-14 15:42:58 +0200

    add default load articles

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -1,15 +1,33 @@
-import {FC} from "react";
+import {FC, useEffect} from "react";
 
 import {SearchBar} from "../SearchBar/SearchBar";
+import {useAppDispatch, useAppSelector} from "../../hooks";
+import {articleActions} from "../../redux";
 
 const Articles: FC = () => {
+    const {articles} = useAppSelector(state => state.articleReducer);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(articleActions.getAll({
+            params: {
+                title_contains: '',
+                summary_contains: '',
+            }
+        }));
+    }, [])
+
     return (
         <div className={'body'}>
             <div className={'wrap'}>
                 <SearchBar/>
 
                 <div className={'articlesContainer'}>
-
+                    {
+                        articles.map((art) => {
+                            return <div key={art.id}>{art.title}</div>;
+                        })
+                    }
                 </div>
             </div>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,22 @@ import {BrowserRouter} from "react-router-dom";
 
 import './index.css';
 import {App} from './App';
+import {setupStore} from "./redux";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+const store = setupStore();
+
 root.render(
     // <React.StrictMode>
+    <Provider store={store}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
+    </Provider>
     // </React.StrictMode>
 );
 
