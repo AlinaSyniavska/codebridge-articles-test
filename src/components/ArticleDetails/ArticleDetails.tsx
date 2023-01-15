dc8dddaad500a59928212099c4eac952d0cd4c0e
AlinaSyniavska <alina22syniavska@gmail.com>
2023-01-15 19:11:51 +0200

    ArticleDetails is done

--- src/components/ArticleDetails/ArticleDetails.tsx
+++ src/components/ArticleDetails/ArticleDetails.tsx
@@ -2,38 +2,58 @@ import {FC} from "react";
 
 import Link from '@mui/material/Link';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import Box from '@mui/material/Box';
 
 import {useAppSelector} from "../../hooks";
 import {muiHelper} from "../../helpers";
-import { ThemeProvider } from "@mui/material";
+import {ThemeProvider} from "@mui/material";
+import CardMedia from "@mui/material/CardMedia";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 
 const ArticleDetails: FC = () => {
     const {articleDetails} = useAppSelector(state => state.articleReducer);
-    // const {title, summary, imageUrl} = articleDetails as IArticle;
 
     return (
         <ThemeProvider theme={muiHelper.createCustomTheme()}>
+            <CardMedia
+                component="img"
+                height="245"
+                image={articleDetails?.imageUrl}
+                alt={articleDetails?.title}
+                sx={{height: 245, position: 'fixed', top: 0, left: 0, zIndex: 1}}
+            />
 
-
-
-
-
-            {articleDetails?.imageUrl}
-            {articleDetails?.title}
-            {articleDetails?.summary}
-
+            <Box sx={{display: 'flex', justifyContent: 'center', margin: '150px 0 35px'}}>
+                <Card sx={{width: 1290, height: 1000, padding: '35px 75px 50px', display: 'flex', justifyContent: 'center', zIndex: 10}}>
+                    <CardContent sx={{display: 'flex', flexDirection: 'column', rowGap: '50px'}}>
+                        <Typography variant="h5" color={'primary'} align={'center'}>
+                            {articleDetails?.title}
+                        </Typography>
+                        <Typography color={'primary'} align={'justify'} sx={{fontSize: '18px'}}>
+                            {articleDetails?.summary}
+                            <br/>
+                            <br/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum est harum nulla quae quo sequi, totam voluptatem. Dolorum, esse, expedita, facere ipsam libero nobis odit perferendis porro quisquam quod sequi.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum est harum nulla quae quo sequi, totam voluptatem. Dolorum, esse, expedita, facere ipsam libero nobis odit perferendis porro quisquam quod sequi.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum est harum nulla quae quo sequi, totam voluptatem. Dolorum, esse, expedita, facere ipsam libero nobis odit perferendis porro quisquam quod sequi.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum est harum nulla quae quo sequi, totam voluptatem. Dolorum, esse, expedita, facere ipsam libero nobis odit perferendis porro quisquam quod sequi.</p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Box>
 
             <Link
                 href="/"
                 variant="body1"
                 underline="none"
                 color={'primary'}
-                sx={{fontWeight: 'bold'}}
+                sx={{fontWeight: 'bold', margin: '0 0 45px 150px', display: 'inline-block'}}
             >
                 <KeyboardBackspaceIcon/> Back to homepage
             </Link>
-
         </ThemeProvider>
     );
 };
